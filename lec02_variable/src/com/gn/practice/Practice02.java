a9584577381e7060f79302971415837e5696ee80
devkskg <devkskg@gmail.com>
2024-11-27 10:20:36 +0900

    [ksk] 철수의 일기

---
+++ lec02_variable/src/com/gn/practice/Practice02.java
@@ -0,0 +1,26 @@
+package com.gn.practice;
+
+public class Practice02 {
+
+	public static void main(String[] args) {
+		
+		boolean isStudent = false;
+		char grade = 'A';
+		byte candy = 30;
+		short walk = 10000;
+		int day = 36500;
+		long future = 365L;
+		float height = 250.5f;
+		double score = 4.0;
+		
+		System.out.println(isStudent);
+		System.out.println(grade);
+		System.out.println(walk);
+		System.out.println(day);
+		System.out.println(future);
+		System.out.println(height);
+		System.out.println(score);
+
+	}
+
+}
