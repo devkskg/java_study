a039d3dcaf842bfdb6075e03f6d4a9d83619b384
study <study@DESKTOP-KJH1RBD>
2024-12-01 00:26:09 +0900

    [ksk] Loop Homework

--- lec04_control/src/com/gn/homework/loop/Practice07.java
+++ lec04_control/src/com/gn/homework/loop/Practice07.java
@@ -5,45 +5,30 @@ import java.util.Scanner;
 public class Practice07 {
 	public void ptc07() {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("정수 : ");
-		int num = sc.nextInt();
 		int i = 1;
+		System.out.print("정수: ");
+		int num = sc.nextInt();
 		
-		if(num<=0) {
-			System.out.println("양수가 아닙니다.");
-			System.out.println("다시 입력하세요. 정수 : ");
-			num = sc.nextInt();
-		} else {
-			while(i <= num) {
-//				if(num <= 0) {
-//				} else {
-					if(i % 2 == 1) {
-						System.out.print("박");
-						i++;
-					} else {
-						System.out.print("수");
-						i++;
-					}
-					
-//				}
-				
+		for(int j = 1; ; j++) {
+			if(num <= 0) {
+				System.out.print("양수가 아닙니다.");
+				System.out.print("다시 입력하세용. 정수 : ");
+				num = sc.nextInt();
+			} else {
+				break;
 			}
 		}
-//		if(num > 0) {
-//			for(int j = 1; j <= num; j++) {
-//				if(j % 2 == 1) {
-//					System.out.print("박");
-//					} else {
-//					System.out.print("수");
-//					}
-//				}
-//			} else {
-//				System.out.println("양수가 아닙니다.");
-//				System.out.println("다시 입력하세요. 정수 : ");
-//				num = sc.nextInt();
-//			}
-//		
-		sc.close();
+		
+
+			for(i = 1; i <= num; i++) {
+				if(i % 2 == 1) {
+					System.out.print("박");
+				} else {
+					System.out.print("수");
+				}
+			}
+		
+	sc.close();	
 	}
 
 
