90b1c5cb9441f33cb7dbd7da27e20d2c192d5791
study <study@DESKTOP-KJH1RBD>
2024-12-09 23:34:51 +0900

    [ksk] 추상 클래스, 인터페이스, homework

--- lec09_polymorphism/src/com/gn/homework04/model/vo/SmartPhone.java
+++ lec09_polymorphism/src/com/gn/homework04/model/vo/SmartPhone.java
@@ -1,7 +1,8 @@
 package com.gn.homework04.model.vo;
 
 public abstract class SmartPhone implements CellPhone, TouchDisplay {
-	
+//	이 클래스의 존재의미가 좀 없는듯한 느낌?
+//	-> 여러 인터페이스를 implements를 받아도 일반적인 상속과 다르게 괜찮다는 걸 보여주려고..?
 	public SmartPhone() {}
 	
 	public void printMaker() {
