1d8f9da065325584a58dd2afd1025ecfeb5843d5
devkskg <devkskg@gmail.com>
2024-12-11 13:42:58 +0900

    [ksk] collection ArrayList commit

--- lec11_collection/src/com/gn/practice02/Run.java
+++ lec11_collection/src/com/gn/practice02/Run.java
@@ -9,8 +9,9 @@ public class Run {
 		String tVar = sc.next();
 		System.out.print("Second(숫자) 입력 : ");
 		int uVar = sc.nextInt();
-
+//		1. pair 타입 객체 생성 + 2. 매개변수 생성자 활용
 		Pair<String, Integer> p = new Pair<String, Integer>(tVar, uVar);
+//		3. getter를 통해 필드값 출력
 		System.out.println("First : " + p.getFirst() + "\nSecond : " + p.getsecond());
 
 		sc.close();
--- lec11_collection/src/com/gn/study/controller/Run.java
+++ lec11_collection/src/com/gn/study/controller/Run.java
@@ -1,5 +1,8 @@
 package com.gn.study.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.gn.study.model.vo.Container;
 
 public class Run {
@@ -61,6 +64,48 @@ public class Run {
 		Container<String> tain = new Container<String>();
 		tain.setItem("안녕하세요.");
 		System.out.println(con.getItem() + tain.getItem());
+		System.out.println("===========");
+
+//		List(ArrayList)
+//		1. 선언과 생성 - 인터페이스를 참조변수로 써야 좋다! 다형성 때문에!
+		List<String> list = new ArrayList<String>(); // Import 필수
+//		2. 추가
+		List<String> meal = new ArrayList<String>();
+		meal.add("아침");
+		meal.add("점심");
+		System.out.println(meal); // List는 이미 toString이 되어있다.
+		meal.add(1, "아점");
+//		meal.add(4, "야식 고?"); 인덱스 3번이 비워진 채로 4번 추가하려니까 IndexOutOfBoundsException 오류가 뜬다.
+//		빈 공간 없게 해야된다!
+//		3. 삭제
+		System.out.println(meal);
+//		meal.remove("간식"); 특정 데이터 쓰면 지울 수 있긴 하는데.. 비추비추.. 객체 들어가면 equals 재정의 하는거 아니면 판단 어렵다.
+		meal.remove(1);
+		System.out.println(meal);
+		meal.clear();
+		System.out.println(meal);
+//		4. 요소 변경
+//		meal.set(0, "ddd"); 비어있는 List에서 0번 요소변경은 안되는 구나 
+//		System.out.println(meal); 
+		List<Integer> nums = new ArrayList<Integer>();
+		nums.add(15);
+		nums.add(10);
+		nums.add(23);
+		System.out.println(nums);
+		nums.set(1, 112233);
+		System.out.println(nums);
+//		5. 요소 조회
+		System.out.println(nums.get(2));
+		for (int i = 0; i < nums.size(); i++) {
+			System.out.println(i + " : " + nums.get(i));
+		}
+
+		for (Integer n : nums) {
+			System.out.println(n);
+		}
+		for (int n : nums) { // wrapper 형태의 데이터를 기본형 자료형으로 바꾸는 것 : Unboxing !!!!!
+			System.out.println(n);
+		}
 
 	}
 }
