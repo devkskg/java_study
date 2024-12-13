7149fc2e760cf48d9231fa69ddddb65455b54fbe
devkskg <devkskg@gmail.com>
2024-12-13 11:41:22 +0900

    [ksk] Map - keySet(), entrySet()

--- lec11_collection/src/com/gn/study/controller/MapRun.java
+++ lec11_collection/src/com/gn/study/controller/MapRun.java
@@ -1,7 +1,10 @@
 package com.gn.study.controller;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import com.gn.study.model.vo.Snack;
 
@@ -24,6 +27,39 @@ public class MapRun {
 		System.out.println(f_nameO);
 		System.out.println(flower.get("lang"));
 
+		System.out.println("===");
+//		6. HashMap 요소 조회 (2)
+//		(1) HashMap 요소들의 key값으로 Set을 만든다. -> key의 제네릭 사용
+		Set<String> keySet = flower.keySet();
+//		(2) Iterator 만들기
+		Iterator<String> iterKey = keySet.iterator();
+//		(3) 손을 집어 넣는다 -> hasNext()
+		while (iterKey.hasNext()) {
+//			(4) 확인한다 -> next()
+			String key = iterKey.next();
+//			System.out.println("key 확인");
+//			System.out.println(key);
+//			System.out.println("value 확인");
+			Object value = flower.get(key);
+//			Object value = (String) flower.get(key); // 이거 왜 오류남??????????????
+			System.out.println(key + ":" + value);
+		}
+		System.out.println("*****");
+//		(1) HashMap 요소들의 key+value 값으로 entrySet 만들기
+		Set<Entry<String, Object>> entrySet = flower.entrySet(); // 형태 잘 보기
+//		(2) Iterator 만들기
+		Iterator<Entry<String, Object>> itEntry = entrySet.iterator();
+//		(3) 손을 집어 넣는다
+		while (itEntry.hasNext()) {
+//			(4) next()
+			Entry<String, Object> entry = itEntry.next();
+			String key = entry.getKey();
+			Object value = entry.getValue();
+//			System.out.println(entry); ??
+			System.out.println(key + "=" + value);
+		}
+		System.out.println("----");
+
 //		3. HashMap<key,객체> 요소 추가
 		Map<String, Snack> snackMap = new HashMap<String, Snack>();
 		snackMap.put("다이제", new Snack("초코맛", 2700));
@@ -40,5 +76,27 @@ public class MapRun {
 //		포테이토칩 양파맛 -> 소금맛
 		snackMap.get("포테이토칩").setFlavor("소금맛");
 		System.out.println(snackMap.get("포테이토칩"));
+
+//		7. HashMap<객체> 요소 조회(2)
+//		(1) keySet()
+		Set<String> keySet1 = snackMap.keySet();
+		Iterator<String> iterKey1 = keySet1.iterator();
+		while (iterKey1.hasNext()) {
+			String key = iterKey1.next();
+			Snack value = snackMap.get(key);
+			System.out.println(key + " ### " + value);
+		}
+
+//		(2) entrySet()
+		Set<Entry<String, Snack>> entSet1 = snackMap.entrySet();
+		Iterator<Entry<String, Snack>> iterEnt1 = entSet1.iterator();
+		while (iterEnt1.hasNext()) {
+			Entry<String, Snack> ent = iterEnt1.next();
+			String key = ent.getKey();
+			Snack value = ent.getValue();
+//			Snack value = snackMap.get(key);
+			System.out.println(key + " %%%% " + value);
+		}
+
 	}
 }
