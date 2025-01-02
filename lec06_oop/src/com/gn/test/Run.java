c173ecb325766e419f5e64c1a3bdbca937af460d
study <study@220.74.61.45>
2025-01-02 23:32:25 +0900

    [ksk] getter, setter, 매개변수 생성자 연습

---
+++ lec06_oop/src/com/gn/test/Run.java
@@ -0,0 +1,9 @@
+package com.gn.test;
+
+public class Run {
+	public static void main(String[] args) {
+		Test t1 = new Test("010123", 500);
+		System.out.println(t1.getPhone());
+		
+	}
+}
---
+++ lec06_oop/src/com/gn/test/Test.java
@@ -0,0 +1,32 @@
+package com.gn.test;
+
+public class Test {
+	private String phone;
+	private int salary;
+	
+	public Test() {}
+
+	public Test(String phone, int salary) {
+		this.phone = phone;
+		this.salary = salary;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	
+	
+}
